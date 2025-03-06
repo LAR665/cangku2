89e841712a275f7ce0a2b1e9421b9d0f187cedc5
LAR665 <3533471542@qq.com>
2025-03-06 14:34:53 +0800

    Add files via upload

---
+++ sizeof 运算符.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a;
+	double b;
+	long c;
+	long int d;
+	char e[10];
+	printf("sizeof a =%d\n",sizeof(a));
+	printf("sizeof b =%d\n",sizeof(b));
+	printf("sizeof c =%d\n",sizeof(c));
+	printf("sizeof d =%d\n",sizeof(d));
+	printf("sizeof e =%d\n",sizeof(e));
+	return 0;
+}
